d7fc40353214190d6e6a51f6f175dbc175e894a3
Andrey P <pfm@live.ru>
2015-11-05 01:32:41 +0300

    first test suits for Balances

--- src/balances.js
+++ src/balances.js
@@ -1,3 +1,123 @@
 function Balances() {
 
+    this.items = [];
+
+    for (var i = 0; i < arguments.length; i++) {
+
+        var b = arguments[i];
+
+        for (code in b) {
+
+            var o = {};
+            o[code] = b[code];
+
+            this.items.push(o)
+        }
+    }
+    this.isEmpty = function () {
+        return this.items.length === 0;
+    };
+
+    this.get = function (code) {
+        var sum = 0;
+
+        for (var i = 0; i < this.items.length; i++) {
+            if (this.items[i].code === code) {
+
+                sum += this.items[i].amount;
+            }
+        }
+        return sum+" "+ this.codeToWord(code);
+    };
+
+    this.add = function (code, amount) {
+        this.items.push({code: code, amount: amount});
+        return this;
+    };
+
+    this.minus = function (code, amount) {
+        this.add(code, -amount);
+        return this;
+    };
+
+    this.codeToWord = function (code) {
+
+        if (code == Balances.CODE_RUR) {
+            return "RUR";
+        }
+
+        if (code == Balances.CODE_USD) {
+            return "USD";
+        }
+
+        if (code == Balances.CODE_EUR) {
+            return "EUR";
+        }
+
+        return code;
+    };
+
+    this.getAmount = function (code) {
+
+        var res = 0;
+
+        for (var i = 0; i < this.items.length; i++) {
+            if (this.items[i].code === code) {
+                res += this.items[i].amount;
+            }
+        }
+        return res;
+    }
 }
+
+Balances.sum = function () {
+
+    var sum = {};
+
+    for (var i = 0; i < arguments.length; i++) {
+
+        var b = arguments[i];
+
+        for (code in b) {
+
+            if (sum.hasOwnProperty(code)) {
+                sum[code] += b[code];
+                continue;
+            }
+
+            sum[code] = b[code];
+        }
+    }
+
+    return sum;
+};
+
+Balances.prototype.toString = function () {
+
+    var res = "";
+
+    var rurs = 0;
+    var eurs = 0;
+    var usds = 0;
+
+    for (var i = 0; i < this.items.length; i++) {
+
+        for (p in this.items[i]) {
+
+            if (p == Balances.CODE_USD)
+                usds += this.items[i][p];
+
+            if (p == Balances.CODE_EUR)
+                eurs += this.items[i][p];
+
+            if (p == Balances.CODE_RUR)
+                rurs += this.items[i][p];
+        }
+    }
+
+    return rurs + " RUR, " + usds + " USD, " + eurs + " EUR";
+};
+
+Balances.CODE_RUR=643;
+Balances.CODE_EUR=980;
+Balances.CODE_USD=810;
--- test/BalancesTests.js
+++ test/BalancesTests.js
@@ -2,9 +2,85 @@ chai.should();
 
 describe("Balances", function () {
 
-    it("should be work", function () {
-
-        true.should.equal(true);
+    it("should be empty", function () {
+        var balances = new Balances();
+        balances.isEmpty().should.equal(true);
     });
 
+    it("should add new balance", function () {
+        var balances = new Balances();
+        balances.add(1, 50);
+        balances.isEmpty().should.equal(false);
+    });
+
+    it("should return RUR balance with one rur", function () {
+        var balances = new Balances();
+        balances.add(1, 50);
+        var rurs = balances.getAmount(1);
+        rurs.should.equal(50)
+    });
+
+    it("should return RUR balance with two rurs", function () {
+        var balances = new Balances();
+        balances.add(1, 50);
+        balances.add(1, 1);
+        var rurs = balances.getAmount(1);
+        rurs.should.equal(51)
+    });
+
+    it("should return correct RUR balance after minus", function () {
+        var balances = new Balances();
+        balances.add(1, 50);
+        balances.minus(1, 1);
+        var rurs = balances.getAmount(1);
+        rurs.should.equal(49);
+    });
+
+    it("test chains add calling", function () {
+        var balances = new Balances();
+        balances.add(1, 50).add(1, 1);
+        var rurs = balances.getAmount(1);
+        rurs.should.equal(51);
+    });
+
+    it("test chains minus calling", function () {
+        var balances = new Balances();
+        balances.add(1, 50).minus(1, 34);
+        var rurs = balances.getAmount(1);
+
+        rurs.should.equal(16);
+    });
+
+    it("get info from toString()", function () {
+        var balances = new Balances();
+        balances.add(Balances.CODE_RUR, 99);
+        var rurs = balances.get(Balances.CODE_RUR);
+        rurs.should.equal("99 RUR");
+    });
+
+
+    it("should save RUR and EUR balances", function () {
+        var balances = new Balances();
+        balances.add(1, 50);
+        balances.add(2, 1);
+        balances.items.length.should.equal(2);
+    });
+
+    it("should log info about balance", function(){
+        //console.log('—ÛÏÏ‡: '+new Balances({643:50,810:10},{980:6},{643:7}))
+        // —ÛÏÏ‡: 57 RUR, 10 USD, 6 EUR
+        var b = new Balances({643:50,810:10},{980:6},{643:7});
+        console.log(b.items);
+        b.toString().should.equal("57 RUR, 10 USD, 6 EUR");
+
+    });
+
+    it("get sum info", function(){
+
+        var sum = Balances.sum({643:50, 810:10},{980:6},{643:7});
+        //Object {643: 57, 840: 10, 980: 6}
+        console.log(sum);
+
+        sum.should.to.deep.equal({643: 57, 810: 10, 980: 6});
+    })
 });
